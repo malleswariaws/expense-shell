bf6125c26e0f54b953ffd66a9a68b728ad14a68d
rishikesh <malleswaraws@gmail.com>
2024-08-29 23:26:17 +0530

    functions

--- mysql.sh
+++ mysql.sh
@@ -8,6 +8,8 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+echo "please enter DB password:"
+read -s mysql_root_password
 
 VALIDATE(){
     if [ $1 -ne 0 ]
@@ -40,10 +42,10 @@ VALIDATE $? "Starting MYSQL Server"
 # VALIDATE $? "Setting up root password"
 
 #Below code will be useful for idempotent nature
-mysql -h db.malleswariaws.online -uroot -pExpenseApp@1 -e 'show databases;' &>>$LOGFILE
+mysql -h db.malleswariaws.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then
-    mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
     VALIDATE $? "MYSQL Root Setup"
 else
     echo -e "MYSQL Root password is already setup...$Y SKIPPING $N"
