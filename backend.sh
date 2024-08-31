c5a6eaa8521aa045adf7c01e95f397336fc30d79
rishikesh <malleswaraws@gmail.com>
2024-08-31 13:12:03 +0530

    functions

--- backend.sh
+++ backend.sh
@@ -60,7 +60,7 @@ VALIDATE $? "Extracted backend code"
 npm install &>>$LOGFILE
 VALIDATE $? "Installing nodejs dependencies" 
 
-cp /home/ec2-user/expense-shell/backend.service /etc/systemd/backend.service &>>$LOGFILE
+cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
 VALIDATE $? "Copied backend service"
 
 systemctl daemon-reload &>>$LOGFILE
